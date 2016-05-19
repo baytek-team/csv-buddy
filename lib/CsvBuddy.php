568bdcfba732514ba546d648c7a959e17cf2248a
Yvon Viger <yvon@baytek.ca>
2016-05-19 17:24:33 -0400

    Housekeeping and added the default value feature

--- lib/CsvBuddy.php
+++ lib/CsvBuddy.php
@@ -5,261 +5,262 @@ namespace Baytek;
 use Exception;
 
 /**
- * Class CsvBuddy
- * @package Baytek\CsvBuddy
+ * Class CsvBuddy.
  */
-Class CsvBuddy {
+class CsvBuddy
+{
+    /**
+     * CSV Delimiter.
+     *
+     * @var string
+     */
+    protected $delimiter = ',';
 
-	/**
-	 * CSV Delimiter
-	 * @var string
-	 */
-	protected $delimiter = ',';
+    /**
+     * CSV Schema Definition.
+     *
+     * @var null
+     */
+    protected $schema = null;
 
-	/**
-	 * CSV Schema Definition
-	 * @var null
-	 */
-	protected $schema = null;
+    /**
+     * CSV Column Headers.
+     *
+     * @var array
+     */
+    protected $headers = [];
 
-	/**
-	 * CSV Column Headers
-	 * @var array
-	 */
-	protected $headers = [];
+    /**
+     * CSV Column Names, not sure if required, we will see.
+     *
+     * @var array
+     */
+    protected $columns = [];
 
-	/**
-	 * CSV Column Names, not sure if required, we will see
-	 * @var array
-	 */
-	protected $columns = [];
+    /**
+     * CSV Data Store.
+     *
+     * @var array
+     */
+    protected $store = [];
 
-	/**
-	 * CSV Data Store
-	 * @var array
-	 */
-	protected $store = [];
+    /**
+     * Index of current row.
+     *
+     * @var int
+     */
+    protected $row = 0;
 
-	/**
-	 * Index of current row
-	 * @var integer
-	 */
-	protected $row = 0;
+    /**
+     * CSV Buddy creation method.
+     *
+     * @param array $table Table Schema in named array format
+     */
+    public function __construct(array $schema)
+    {
+        //Save the schema
+        $this->schema = $schema;
 
-	/**
-	 * CSV Buddy creation method
-	 * @param Array $table Table Schema in named array format
-	 */
-	public function __construct(Array $schema)
-	{
-		//Save the schema
-		$this->schema = $schema;
+        // Check to see that this is a sequential array and not a named key array
+        $sequential = $this->isSequential($schema);
 
-		// Check to see that this is a sequential array and not a named key array
-		$sequential = $this->isSequential($schema);
+        if ($sequential) {
+            foreach ($schema as $column) {
+                array_push($this->headers, $column);
+                array_push($this->columns, $column);
+                $this->store[] = [];
+            }
+        } else {
+            foreach ($schema as $column => $parameters) {
+                array_push($this->headers, isset($parameters['header']) ? $parameters['header'] : $column);
+                array_push($this->columns, $column);
+                $this->store[] = [];
+            }
+        }
+    }
 
-		if($sequential) {
-			foreach($schema as $column) {
-				array_push($this->headers, $column);
-				array_push($this->columns, $column);
-				$this->store[] = [];
-			}
-		}
-		else {
-			foreach($schema as $column => $parameters) {
-				array_push($this->headers, isset($parameters['header']) ? $parameters['header'] : $column);
-				array_push($this->columns, $column);
-				$this->store[] = [];
-			}
-		}
-	}
+    /**
+     * toString will return a valid CSV string.
+     *
+     * @return [string] CSV Result
+     */
+    public function __toString()
+    {
+        return $this->render();
+    }
 
-	/**
-	 * To String will return a valid CSV string
-	 * @return [string] CSV Result
-	 */
-	public function __toString()
-	{
-		return $this->render();
-	}
+    /**
+     * Magic method for setting a column in the data store
+     *
+     * @param string $column Column to set
+     * @param mixed  $value  Data to set into column
+     */
+    public function __set($column, $value)
+    {
+        $this->put($column, $value);
+    }
 
-	/**
-	 * To String will return a valid CSV string
-	 * @return null
-	 */
-	public function __set($column, $value)
-	{
-		$this->put($column, $value);
-	}
+    /**
+     * setColumn method takes a key and value pair and sets that to the data store.
+     *
+     * @param string $column Column to set
+     * @param mixed  $value  Data to set into column
+     */
+    public function setColumn($column, $value)
+    {
+        $this->put($column, $value);
 
-	/**
-	 * Creates the actual CSV file based on columns and rows sent
-	 * @return [string] CSV Result
-	 */
-	public function render()
-	{
-		$this->print_table($this->store);
+        return $this;
+    }
 
-		ob_start();
+    /**
+     * addRow takes an array of fields to set into the data store.
+     *
+     * @param array $columns An named array of values to be set in the data store
+     *
+     * @return $this Return self to allow for chaining
+     */
+    public function addRow($columns)
+    {
+        foreach ($columns as $column => $value) {
+            $this->put($column, $value);
+        }
 
-		$handle = fopen('php://output', 'r+');
+        $this->endRow();
 
-		if (count($this->headers))
-		{
-			fputcsv($handle, $this->headers, $this->delimiter);
-		}
+        return $this;
+        // return call_user_func_array(array($this, 'fill'), func_get_args());
+    }
 
-		for($x = 0; $x <= $this->row; $x++)
-		{
-			$row = [];
-			foreach($this->columns as $column)
-			{
-				if(!isset($this->store[$x][$column])) {
-					$value = $this->defaults($column, $x);
-				}
-				else {
-					$value = $this->store[$x][$column];
-				}
+    /**
+     * [put description].
+     *
+     * @param string $column Column to set
+     * @param mixed  $value  Data to set into column
+     *
+     * @return [type] [description]
+     */
+    private function put($column, $value)
+    {
+        $this->validate($column, $value);
 
-				array_push($row, $value);
-			}
+        if (!empty($this->store[$this->row][$column])) {
+            throw new Exception('Cell already contains data, you cannot re-populate cells for the time being');
+        }
 
-			fputcsv($handle, $row, $this->delimiter);
-		}
+        $this->store[$this->row][$column] = $value;
 
-		return ob_get_clean();
-	}
+        return $this;
+    }
 
-	/**
-	 * Default will check based on the column name if there is a default value if non is set
-	 * @param  [type] $column [description]
-	 * @return mixed         [description]
-	 */
-	public function defaults($column, $row)
-	{
-		if(isset($this->schema[$column]['default'])) {
+    // $this->print_table($this->store);
 
-			$default = $this->schema[$column]['default'];
+    /**
+     * Creates the actual CSV file based on columns and rows sent.
+     *
+     * @return [string] CSV Result
+     */
+    public function render()
+    {
+        ob_start();
 
-			if(is_callable($default)) {
-				return $default($row);
-			}
-			else {
-				return $default;
-			}
-		}
-		else {
-			return null;
-		}
-	}
+        $handle = fopen('php://output', 'r+');
 
-	/**
-	 * [isSequential description]
-	 * @param  [type]  $array [description]
-	 * @return boolean        [description]
-	 */
-	private function isSequential($array)
-	{
-		return array_keys($array) === range(0, count($array) - 1);
-	}
+        if (count($this->headers)) {
+            fputcsv($handle, $this->headers, $this->delimiter);
+        }
 
-	private function put($column, $value)
-	{
-		if(!empty($this->store[$this->row][$column])) {
-			throw new Exception('Cell already contains data, you cannot re-populate cells for the time being');
-		}
+        for ($x = 0; $x <= $this->row; ++$x) {
+            $row = [];
+            foreach ($this->columns as $column) {
+                if (!isset($this->store[$x][$column])) {
+                    $value = $this->defaults($column, $x);
+                } else {
+                    $value = $this->store[$x][$column];
+                }
 
-		$this->store[$this->row][$column] = $value;
+                array_push($row, $value);
+            }
 
-		return $this;
-	}
+            fputcsv($handle, $row, $this->delimiter);
+        }
 
-	/**
-	 * New row will iterate through all of the columns and null fill each unfilled column and increment the row value
-	 * @return $this for chaining
-	 */
-	public function endRow()
-	{
-		//Add some checks to ensure that the current row isn't empty, causing empty rows
-		$this->row ++;
+        return ob_get_clean();
+    }
 
-		foreach($this->columns as $column)
-		{
-			$this->store[$this->row][$column] = null;
-		}
+    /**
+     * Default will check based on the column name if there is a default value if non is set.
+     *
+     * @param string $column Column to check the default value for
+     *
+     * @return mixed The default value
+     */
+    public function defaults($column, $row)
+    {
+        if (isset($this->schema[$column]['default'])) {
+            $default = $this->schema[$column]['default'];
 
-		return $this; // Allow for method chaining
-	}
+            if (is_callable($default)) {
+                return $default($row);
+            } else {
+                return $default;
+            }
+        } else {
+            return;
+        }
+    }
 
+    /**
+     * isSequential Checks to see if the array is sequential or is named keys
+     *
+     * @param array $array The array to check
+     *
+     * @return bool isSequential?
+     */
+    private function isSequential($array)
+    {
+        return array_keys($array) === range(0, count($array) - 1);
+    }
 
+    /**
+     * endRow will iterate through all of the columns and null fill each unfilled column and increment the row value.
+     *
+     * @return $this for chaining
+     */
+    public function endRow()
+    {
+        //Add some checks to ensure that the current row isn't empty, causing empty rows
+        ++$this->row;
 
-	/**
-	 * Public method for filling a column with data
-	 * @return $this for chaining
-	 */
-	// protected function fill() //$cell, $value
-	// {
-	// 	$args = func_get_args();
-	// 	$data = is_array( $args[0] ) ? $args[0] : $args;
+        foreach ($this->columns as $column) {
+            $this->store[$this->row][$column] = null;
+        }
 
-	// 	$sequential = $this->isSequential($data);
+        return $this; // Allow for method chaining
+    }
 
-	// 	if($sequential) {
-	// 		$this->put($data[0], $data[1])->endRowCheck();
-	// 	}
-	// 	else {
-	// 		foreach($data as $column => $value) {
-	// 			$this->put($column, $value);
-	// 		}
-	// 	}
+    ///////////////////////////////////////////////////////////////////////////
+    /// DEBUGGING FUNCTIONS ///////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
 
-	// 	return $this; // Allow for method chaining
-	// }
+    public function print_table($table)
+    {
+        echo '<table border="1">';
+        array_walk($table, array('self', 'print_row'));
+        echo '</table>';
+    }
 
+    private function print_row(&$item)
+    {
+        echo '<tr>';
+        array_walk($item, array('self', 'print_cell'));
+        echo '</tr>';
+    }
 
-	public function setColumn($column, $value)
-	{
-		$this->put($column, $value);
-		return $this;
-	}
-
-	public function addRow($columns)
-	{
-		foreach($columns as $column => $value) {
-			$this->put($column, $value);
-		}
-
-		$this->endRow();
-		return $this;
-		// return call_user_func_array(array($this, 'fill'), func_get_args());
-	}
-
-
-	///////////////////////////////////////////////////////////////////////////
-	/// DEBUGGING FUNCTIONS ///////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////
-
-
-
-	public function print_table($table)
-	{
-		echo '<table border="1">';
-		array_walk($table, array('self', 'print_row'));
-		echo '</table>';
-	}
-
-	private function print_row(&$item)
-	{
-		echo('<tr>');
-		array_walk($item, array('self', 'print_cell'));
-		echo('</tr>');
-	}
-
-	private function print_cell(&$item)
-	{
-		echo('<td>');
-		echo($item);
-		echo('</td>');
-	}
-
+    private function print_cell(&$item)
+    {
+        echo '<td>';
+        echo $item;
+        echo '</td>';
+    }
 }
